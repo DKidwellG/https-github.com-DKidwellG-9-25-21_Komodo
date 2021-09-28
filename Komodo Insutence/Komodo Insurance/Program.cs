93d4a4e6957e713c63814d16fab73cd6e4e58a39
DKidwellG <84405900+DKidwellG@users.noreply.github.com>
2021-09-28 18:12:47 -0400

    Add files via upload

---
+++ Komodo Insutence/Komodo Insurance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komodo_Insurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UI program = new UI();
+            program.Run();
+        }
+    }
+
+}
+
+
+
+
---
+++ Komodo Insutence/Komodo Insurance/teamRepo.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komodo_Insurance
+{
+    class TeamRepo
+    {
+        public List<DevRepo> TeamMembers { get; set; }
+        public string TeamName { get; set; }
+        public short TeamID { get; set; }
+
+        public TeamRepo() { }
+        public TeamRepo(string teamName, short teamId)
+        {
+            TeamMembers = new List<DevRepo>();
+            TeamName = teamName;
+            TeamID = teamId;
+        }
+        private List<TeamRepo> _listOfTeam = new List<TeamRepo>();
+        //create
+        public void AddTeamToList(TeamRepo Team)
+        {
+            _listOfTeam.Add(Team);
+        }
+
+        //read
+        public List<TeamRepo> GetTeam()
+        {
+            return _listOfTeam;
+        }
+
+        //update
+        public bool UpdateExistingTeam(short originalTeam, TeamRepo newTeam)
+        {
+            //find
+            TeamRepo oldTeam = GetTeamById(originalTeam);
+
+            //update
+            if (oldTeam != null)
+            {
+                oldTeam.TeamMembers = newTeam.TeamMembers;
+                oldTeam.TeamName = newTeam.TeamName;
+                oldTeam.TeamID = newTeam.TeamID;                
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        //delete
+        public bool RemoveTeamFromList(short id)
+        {
+            TeamRepo content = GetTeamById(id);
+
+            if (content == null)
+            {
+                return false;
+            }
+            int initialCount = _listOfTeam.Count;
+            _listOfTeam.Remove(content);
+            if (initialCount > _listOfTeam.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        //helper
+        private TeamRepo GetTeamById(short id)
+        {
+            foreach (TeamRepo content in _listOfTeam)
+            {
+                if (content.TeamID == id)
+                {
+                    return content;
+                }
+            }
+            return null;
+        }
+
+    }
+}
+
